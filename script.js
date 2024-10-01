cb43e67d19db5bf3b337710973b7ebbd81452cad
user510DAAAA <131327154+user510DAAAA@users.noreply.github.com>
2024-10-01 10:39:59 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// JavaScript
+
+// 定数の定義
+const loading = document.getElementById('loading');
+
+// cssクラスを追加する関数
+function animation(){
+  loading.classList.add('loaded');
+}
+//画面が読み込まれたら animation を呼び出す
+window.addEventListener('load', animation);
+// window.addEventListener('load', animation);
+
+// 指定秒後に動作させる
+window.setTimeout( animation, 3000 );
